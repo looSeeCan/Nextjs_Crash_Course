705e5f6c6c66643fa09305ada01db983bdef5621
looSeeCan <95648904+looSeeCan@users.noreply.github.com>
2022-06-17 19:12:27 -0500

    good
    did this to figure out some shit on that prisma  tutorial. It helped alot. IDk if I am going to continue it though

---
+++ next-crash-course/components/Layout.js
@@ -0,0 +1,15 @@
+import styles from "../styles/Layout.module.css";
+
+const Layout = ({children}) => {
+    console.log(children);
+    return(
+        <div className={styles.container}>
+            <main className={styles.main}>
+                <h1>Hello</h1>
+                {children}
+            </main>    
+        </div>
+    )
+}
+
+export default Layout;
--- next-crash-course/pages/_app.js
+++ next-crash-course/pages/_app.js
@@ -1,7 +1,18 @@
 import '../styles/globals.css'
+import Layout from '../components/Layout';
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	// console.log(Component);
+	// console.log(pageProps)
+	return (
+		<div>
+			<Layout>
+				<Component {...pageProps} />
+			</Layout>
+		</div>
+		)
+
 }
 
 export default MyApp
